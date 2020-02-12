f1a13cbbcd3a9e5002bc461c47231d5f60f9c512
Ihar Sakun <watsonik@gmail.com>
2020-02-12 13:28:40 +0300

    try command

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const yargs = require('yargs');
 
-yargs.command('$0 [name]', 'start the server', (yargs) => {
-    yargs
-        .positional('name', {
-            describe: 'name to display',
-            default: 'world'
-        })
-        .option('times', {
-            alias: 't',
-            type: 'number',
-            default: 1,
-            description: 'number of times the message is logged'
-        })
-}, (argv) => {
-    for (let i = 0; i < argv.times; i++) {
-        console.log(`Hello ${argv.name}!`)
-    }
-})
+yargs.command('get', 'make a get HTTP request', {
+        url: {
+            alias: 'u',
+            default: 'http://yargs.js.org/'
+        }
+    }, (args) => console.log(args.url)
+)
+    .help()
+    .argv
 yargs.parse();
